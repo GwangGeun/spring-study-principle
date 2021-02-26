536fdb3113aef655651c4d215b507383294cc500
GwangGeun <jgg0328@gmail.com>
2021-02-26 17:52:25 +0900

    web scope study

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,9 +1,8 @@
 package hello.core.scope;
 
-import lombok.RequiredArgsConstructor;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
